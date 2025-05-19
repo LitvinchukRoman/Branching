14061b045f0f417ba877f867aa67e6c7fa130ab8
litvi <litvin4uk.roman@gmail.com>
2025-05-19 10:53:40 +0300

    beginning of branching

---
+++ git-1/Something.java
@@ -0,0 +1,10 @@
+public class Something {
+
+    private int value;
+
+    private String name;
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
